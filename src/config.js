baba6bba1ecf37e75f2de31136952160ac37a89d
Karoline Alencar <karolinedalencar@gmail.com>
2019-08-30 09:37:59 -0300

    Add authorization flow

--- src/config.js
+++ src/config.js
@@ -1,2 +1,9 @@
-// eslint-disable-next-line import/prefer-default-export
+const TOKEN_API =
+  'BQBwPYupXI1tTJTdFyVVzAxRtupxrezsD5VGgQQFR1lyvQC73f72GCuiBdlpPRDvWhZhMLkjZJMNYZtcigx8aeAO27qJlbjkG4u7yN-7Kwcq78h5fhuHEdaHGqZ9tP8FjF9sWHzqCAeVDg';
 export const API_URL = 'https://api.spotify.com/v1';
+
+export const HEADERS = {
+  headers: {
+    Authorization: `Bearer ${TOKEN_API}`,
+  },
+};
