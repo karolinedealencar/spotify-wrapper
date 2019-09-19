4fb673f72f84e24933a12f92f95459898ba2e627
Karoline Alencar <karolinedalencar@gmail.com>
2019-09-18 21:24:51 -0300

    Refactor search method

--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
-import { search, searchArtists, searchAlbums, searchPlaylists } from "./search";
-import album from "./album";
-
-/* module.exports = {
-  search,
-  searchArtists,
-  searchAlbums,
-  searchPlaylists,
-  getAlbum,
-  getAlbums,
-  getAlbumTracks,
-};*/
-
-import { API_URL } from "./config";
+import search from './search';
+import album from './album';
+import { API_URL } from './config';
 
 export default class SpotifyWrapper {
   constructor(options) {
     this.apiURL = options.apiURL || API_URL;
     this.token = options.token;
     this.album = album.bind(this)();
+    this.search = search.bind(this)();
   }
 
   request(url) {
     const headers = {
       headers: {
-        Authorization: `Bearer ${this.token}`
-      }
+        Authorization: `Bearer ${this.token}`,
+      },
     };
 
     return fetch(url, headers);
